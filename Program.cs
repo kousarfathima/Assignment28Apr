628d2f78d313886895650cf155be73686a6b08a3
kousarfathima <128119973+kousarfathima@users.noreply.github.com>
2023-06-12 17:43:54 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    delegate void GreetingDelegate(string name);
+
+    static void Main()
+    {
+        GreetingDelegate greetDelegate = delegate (string name)
+        {
+            Console.WriteLine("Hello, " + name + "!");
+        };
+
+        greetDelegate("John");
+
+        Console.ReadKey();
+    }
+}
